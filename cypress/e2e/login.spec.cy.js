cf8ade1d2a659e7f7832f721318c0f707a19f9df
BrunoChagas <brunocsega@gmail.com>
2026-01-27 15:34:22 -0300

    baseUrl understanding and using

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -2,7 +2,7 @@
 //Pular teste: it.skip... -> it.skip('Login - Success'......
 //Algumas observações: Eu não fiz o melhor modo em alguns lugares do código, mas foi justamente pensando que existe mais de uma maneira de se fazer as coisas
 
-import userData from '../fixtures/users/user-data.json'
+import userData from '../fixtures/users/user-data.json' //esses dados ficam separados para tornar o código mais limpo, então fica na pasta fixtures
 
 const selectorsList = {
   userNameField: '[name="username"]',
@@ -12,7 +12,7 @@ const selectorsList = {
 
 describe('Orange HRM Tests', () => {
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login') // cy.visit vai sempre procurar o baseUrl (baseUrl dentro da configuração pq ela não é um dado de teste e sim um endereço onde será feito os testes, uma configuração) e completar com o que "sobrou" (/auth/login)
     cy.get(selectorsList.userNameField).type(userData.userSuccess.name)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -21,7 +21,7 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail - Username', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.userNameField).type(userData.userFail.failField)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -29,7 +29,7 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail - Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.userNameField).type(userData.userSuccess.name)
     cy.get(selectorsList.passwordField).type(userData.userFail.failField)
     cy.get(selectorsList.loginButton).click()

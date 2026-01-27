6f82cc11f3c270e9dc10e677c02497ff79ee0ffd
BrunoChagas <brunocsega@gmail.com>
2026-01-27 15:22:01 -0300

    Adding userData json + changing some fields

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,10 +1,10 @@
 
 //Pular teste: it.skip... -> it.skip('Login - Success'......
+//Algumas observações: Eu não fiz o melhor modo em alguns lugares do código, mas foi justamente pensando que existe mais de uma maneira de se fazer as coisas
+
+import userData from '../fixtures/users/user-data.json'
 
 const selectorsList = {
-  name: 'Admin',
-  password: 'admin123',
-  failField: 'test',
   userNameField: '[name="username"]',
   passwordField: '[name="password"]',
   loginButton: '.oxd-button',
@@ -13,8 +13,8 @@ const selectorsList = {
 describe('Orange HRM Tests', () => {
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.userNameField).type(selectorsList.name)
-    cy.get(selectorsList.passwordField).type(selectorsList.password)
+    cy.get(selectorsList.userNameField).type(userData.userSuccess.name)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     //cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //pathname aqui se refere ao caminho depois da url principal -> passa parâmetro "deve ser igual a" 
     cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard') //funciona pensando do mesmo modo da linha de cima, ele está fazendo uma verificação para ver se existe - no caso a palavra "Dashboard"
@@ -22,16 +22,16 @@ describe('Orange HRM Tests', () => {
 
   it('Login - Fail - Username', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.userNameField).type(selectorsList.failField)
-    cy.get(selectorsList.passwordField).type(selectorsList.password)
+    cy.get(selectorsList.userNameField).type(userData.userFail.failField)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials') //aqui estou passando o texto do alerta, mas não é interessante pois esse texto poderia ser alterado para Invalid Password por exemplo
   })
 
   it('Login - Fail - Password', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.userNameField).type(selectorsList.name)
-    cy.get(selectorsList.passwordField).type(selectorsList.failField)
+    cy.get(selectorsList.userNameField).type(userData.userSuccess.name)
+    cy.get(selectorsList.passwordField).type(userData.userFail.failField)
     cy.get(selectorsList.loginButton).click()
     cy.get('.oxd-alert') //encontrado usando o CSS Selector
   })
